f3cac25eb54553b6a8eadce726668cd62f6a96c9
eddy <18621781488@163.com>
2022-10-13 18:12:18 +0800

    http端增加简易认证

--- internal/server/http.go
+++ internal/server/http.go
@@ -3,6 +3,7 @@ package server
 import (
 	// v1 "kylin-uploader/api/helloworld/v1"
 
+	"context"
 	"fmt"
 	"io"
 	v1 "kylin-uploader/api/v1"
@@ -13,13 +14,32 @@ import (
 	"path"
 	"strconv"
 
+	"github.com/go-kratos/kratos/v2/errors"
 	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-kratos/kratos/v2/middleware"
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
+	"github.com/go-kratos/kratos/v2/transport"
 	"github.com/go-kratos/kratos/v2/transport/http"
 	"github.com/gorilla/mux"
 )
 
 var chunkBasicDir string
+
+func Authenticate(m middleware.Handler) middleware.Handler {
+	return func(ctx context.Context, req interface{}) (interface{}, error) {
+		header, ok := transport.FromServerContext(ctx)
+		if ok {
+			fmt.Println()
+			if header.RequestHeader().Get("Auth") == "61646d696e61646d696e" {
+				return m(ctx, req)
+			} else {
+				return nil, errors.Unauthorized("UNAUTHORIZED", "Auth failed")
+			}
+		} else {
+			return nil, errors.Unauthorized("UNAUTHORIZED", "Auth failed")
+		}
+	}
+}
 
 // NewHTTPServer new a HTTP server.
 func NewHTTPServer(c *conf.Server, chunk *service.ChunkService, logger log.Logger) *http.Server {
@@ -38,6 +58,9 @@ func NewHTTPServer(c *conf.Server, chunk *service.ChunkService, logger log.Logge
 	if c.Http.Timeout != nil {
 		opts = append(opts, http.Timeout(c.Http.Timeout.AsDuration()))
 	}
+	opts = append(opts, http.Middleware(
+		Authenticate,
+	))
 	srv := http.NewServer(opts...)
 	// v1.RegisterGreeterHTTPServer(srv, greeter)
 	v1.RegisterChunkHTTPServer(srv, chunk)
